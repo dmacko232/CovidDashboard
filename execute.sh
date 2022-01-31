45f3477997836a6f230405f2fbdd200a7c33e195
Dominik Macko <xmacko1@fi.muni.cz>
2022-01-31 11:23:35 +0100

    Heroku Procfile

--- execute.sh
+++ execute.sh
@@ -2,4 +2,4 @@
 python3 -m pip install -r requirements.txt
 #python3 scripts/prepare.py data/raw_total.csv data/prepared_total.csv
 #python3 src/app.py
-gunicorn src/app:server
+gunicorn src/app:app
